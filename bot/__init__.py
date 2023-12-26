f1aa8920a366e007f01e52ede26b602db3e77165
REYAD X SHIPU <129636176+BINOD-XD@users.noreply.github.com>
2023-12-26 14:38:26 +0600

    Add files via upload

---
+++ bot/__init__.py
@@ -0,0 +1,7 @@
+# (c) @teamrxs
+
+from dotenv import load_dotenv
+import bot.client
+
+load_dotenv()
+bot = bot.client.Client()
---
+++ bot/__main__.py
@@ -0,0 +1,7 @@
+# (c) @teamrxs
+
+import bot
+
+
+if __name__ == "__main__":
+    bot.bot.run()
